775813b1bf72b501bbf297a08dc9ac908bc0fa59
signal451 <uwugoth266@gmail.com>
2024-06-18 20:42:10 +0800

    minor: Command note

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import CommandNote from "./_components/Command"
 import InputWrapper from "./_components/Wrapper"
 
 const monkeyPage = () => {
@@ -7,6 +8,9 @@ const monkeyPage = () => {
     return (
         <div className="flex flex-col pb-20 h-full my-auto px-5 md:px-10">
           <InputWrapper/>
+          <div className="mt-8 text-center justify-center items-center flex">
+            <CommandNote/>
+          </div>
         </div>
     )
 }
