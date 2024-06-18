5a45f0794c787222af22724cfcad47806e0583f2
signal451 <uwugoth266@gmail.com>
2024-06-18 20:42:58 +0800

    chore

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,8 +3,6 @@ import InputWrapper from "./_components/Wrapper"
 
 const monkeyPage = () => {
 
-  // I might as well load data from here
-
     return (
         <div className="flex flex-col pb-20 h-full my-auto px-5 md:px-10">
           <InputWrapper/>
