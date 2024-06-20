9d4504edef016101cb279142688cd1e659ae225d
signal451 <uwugoth266@gmail.com>
2024-06-20 19:54:38 +0800

    add: middleware url protection

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,21 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from './utils/supabase/middleware'
+import { NextResponse, type NextRequest } from 'next/server';
+import { createClient } from './utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const { supabase, response } = createClient(request);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  // !user - true (hereglegch nevtreegue gsn ug), 
+
+  if (!user && request.nextUrl.pathname.startsWith('/account')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  return response;
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
-}
+};
